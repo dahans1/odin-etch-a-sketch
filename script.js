0e45e6ac402b699597bda1edad72a76a8f978719
San Dahan <sandahan007@gmail.com>
2025-09-02 16:09:35 -0400

    Set a "hover" effect to change color on mouseenter

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function getRandomColor() {
+    const hexValues = '0123456789ABCDEF';
+    var color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += hexValues[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
 function createGrid(size) {
     const container = document.querySelector('#container')
     for (let i = 0; i < size; i++) {
@@ -5,10 +16,25 @@ function createGrid(size) {
         for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
+
+            // calculate space (960 px wide in total)
+            const space = Math.floor(960 / size);
+            square.style.width = `${space}px`;
+            square.style.height = `${space}px`;
+
+            square.dataset.colored = "false";
+            square.addEventListener("mouseenter", () => {
+                console.log(square.style.backgroundColor);
+                if (square.dataset.colored === 'false') {
+                    square.style.backgroundColor = getRandomColor();
+                    square.dataset.colored = "true";
+                }
+            });
+            
             innerContainer.appendChild(square);
         }
         container.appendChild(innerContainer);
     }
 }
 
-createGrid(16);
+createGrid(32);
